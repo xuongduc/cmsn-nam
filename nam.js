7b93d87ebf68e793bc100f622cfb43af093be4ee
xuongduc <phuongxuongduc29052005@gmail.com>
2024-01-26 22:34:42 +0700

    first-test

---
+++ nam.js
@@ -0,0 +1,10 @@
+
+const click = document.getElementById('snzz');
+click.addEventListener ("click", (clicked) => {
+    var haha = document.getElementById('ha');
+    haha.muted = !haha.muted;
+
+    alert(" t xạo loz mà nó tin thệc kìa chời ơi là chời, khờ dữ zậy, kkkkkk cừi khùm");
+
+});
+
